77de5148f3ca61bccb27a630f99c92cebd4f3627
Tilkeii <florian@gouarin.info>
2020-04-11 17:58:35 +0200

    chore(build & ci): travis update to include Heroku pipeline and add semantic-release

---
+++ build.js
@@ -0,0 +1,32 @@
+(async () => {
+  const { exec } = require('child_process');
+
+  let child;
+
+  const node_env = process.env.NODE_ENV.trim();
+
+  if(node_env === 'development') {
+    child = exec('npm run build');
+  } 
+  else if(node_env == 'production') {
+    child = exec('npm run build-prod');
+  } 
+  else {
+    throw new Error('NODE_ENV not correctly defined');
+  }
+
+  child.stdout.on('data', (data) => {
+    console.log('[EXEC-INFO]', data);
+  })
+
+  child.stderr.on('data', (error) => {
+    console.log('[EXEC-ERROR]', error)
+  });
+
+  child.on('close', (code) => {
+    console.log(`[EXEC-END] child process exited with code ${code}`);
+  });
+
+})().catch((err) => {
+  console.error('[BUILD-ERROR]', err);
+});
