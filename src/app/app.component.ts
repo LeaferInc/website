e72554404930cb0f6db218aac7b1fe33af29a3a7
Tilkeii <florian@gouarin.info>
2020-04-24 21:00:31 +0200

    refactor(user persistence): add user persistence and update AuthInterceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './core/services/auth/auth.service';
+import { User } from './shared/models/user/user';
+import { UserAuth } from './shared/models/auth/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Leafer';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const token = this.authService.getTokenFromLocalStorage();
+    if(token) {
+      this.authService
+        .getUserFromToken(token)
+        .subscribe((user: User) => {
+          const userAuth: UserAuth = {
+            token: token,
+            user: user
+          };
+
+          this.authService.setUserAuth(userAuth);
+        });
+    }
+  }
 }
--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, of, from } from 'rxjs';
+import { BehaviorSubject, of, from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UserAuth } from 'src/app/shared/models/auth/auth';
+import { User } from 'src/app/shared/models/user/user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ import { UserAuth } from 'src/app/shared/models/auth/auth';
 export class AuthService {
 
   static readonly AUTH_URL = 'auth';
-  public userAuth: BehaviorSubject<UserAuth> = new BehaviorSubject<UserAuth>({} as UserAuth);
+  private userAuth: BehaviorSubject<UserAuth> = new BehaviorSubject<UserAuth>({} as UserAuth);
 
   constructor(
     private http: HttpClient,
@@ -20,6 +21,7 @@ export class AuthService {
     return this.http.post<UserAuth>(`${AuthService.AUTH_URL}/login`, { username: username, password: password })
       .pipe(
         map(userAuth => {
+          localStorage.setItem('token', userAuth.token);
           this.userAuth.next(userAuth);
           return userAuth;
         })
@@ -30,6 +32,25 @@ export class AuthService {
    * Not yet implemented
    */
   logout() {
+    localStorage.removeItem('token');
+    this.userAuth.next(null);
     throw new Error('Not yet implemented');
   }
+
+  getUserAuth(): Observable<UserAuth> {
+    return this.userAuth.asObservable();
+  }
+
+  getTokenFromLocalStorage(): string {
+    return localStorage.getItem('token');
+  }
+
+  getUserFromToken(token: string): Observable<User> {
+    return this.http.get<User>(`${AuthService.AUTH_URL}/me`);
+  }
+
+  setUserAuth(userAuth: UserAuth): void {
+    this.userAuth.next(userAuth);
+  }
+  
 }
