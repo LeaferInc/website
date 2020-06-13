229aac4aae0813939f89e4b8d4b17494e8f2c566
ddaninthe <d.daninthelopez@gmail.com>
2020-06-13 15:19:07 +0200

    Tests on event search

--- src/app/modules/event/event-routing.module.ts
+++ src/app/modules/event/event-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateEventComponent } from './pages/create-event/create-event.component';
 import { EventsListComponent } from './pages/events-list/events-list.component';
 import { EventInfosComponent } from './pages/event-infos/event-infos.component';
-import { EventsSearchComponent } from './pages/events-search/events-search.component';
+import { EventsSearchComponent, SearchType } from './pages/events-search/events-search.component';
 
 const routes: Routes = [
   { path: '', component: EventsListComponent },
   { path: 'create', component: CreateEventComponent },
-  { path: 'search', component: EventsSearchComponent },
+  { path: 'search/closest', component: EventsSearchComponent, data: { searchType: SearchType.LOCATION } },
+  { path: 'search/date', component: EventsSearchComponent, data: { searchType: SearchType.DATE } },
+  { path: 'search', redirectTo: 'search/date', pathMatch: 'full'},
   { path: ':id', component: EventInfosComponent }
 ];
 
--- src/app/modules/event/pages/events-search/events-search.component.ts
+++ src/app/modules/event/pages/events-search/events-search.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from 'src/app/core/services/event/event.service';
 import { Event } from '../../../../shared/models/event/event.model';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { SearchEvent } from 'src/app/shared/models/event/searchEvent.model';
 import { HttpErrorResponse } from '@angular/common/http';
 import { UtilsService } from 'src/app/core/services/utils/utils.service';
+import { Location } from 'src/app/shared/models/location/location.model';
+import { ActivatedRoute } from '@angular/router';
+
+export enum SearchType {
+  LOCATION,
+  DATE,
+}
 
 @Component({
   selector: 'app-events-search',
@@ -13,28 +20,46 @@ import { UtilsService } from 'src/app/core/services/utils/utils.service';
 })
 export class EventsSearchComponent implements OnInit {
 
-  dateForm: FormGroup; // Search for date
+  searchType: SearchType = SearchType.DATE; // Default to date search
 
+  dateForm: FormGroup; // Form for date search
   searchedEvents: Array<Event> = []; // Searched events
-  searched: boolean = false; // True if a search has been made
+  searched: boolean = false; // True if the search has been done
 
-  constructor(private eventService: EventService, private builder: FormBuilder) { }
+  constructor(private eventService: EventService, private route: ActivatedRoute, private utils: UtilsService) { }
 
   ngOnInit(): void {
-    // Default start date is next day
-    const startDate: Date = new Date();
-    startDate.setDate(startDate.getDate() + 1);
-    startDate.setMinutes(0);
-    this.dateForm = new FormGroup({
-      startDate: new FormControl(UtilsService.dateToJSONLocal(startDate).slice(0, 16), [Validators.required]),
+    this.route.data.subscribe((data: { searchType: SearchType }) => {
+      this.searchType = data.searchType;
     });
+
+
+    // Date Search
+    if (this.searchType == SearchType.DATE) {
+      // Default start date is next day
+      const startDate: Date = new Date();
+      startDate.setDate(startDate.getDate() + 1);
+      startDate.setMinutes(0);
+      this.dateForm = new FormGroup({
+        startDate: new FormControl(UtilsService.dateToJSONLocal(startDate).slice(0, 16), [Validators.required]),
+      });
+    }
+
+    // Location Search
+    if (this.searchType == SearchType.LOCATION) {
+      this.utils.getCurrentLocation()
+        .then((loc: Location) => {
+          const search = SearchEvent.fromLocation(loc.lat, loc.long);
+          this.doSearch(search);
+        }).catch(err => console.log(err));
+    }
   }
 
   /**
-   * Search events by closest location
+   * True if search type is date search
    */
-  searchClosest(): void {
-    console.log("closest");
+  isDateSearch(): boolean {
+    return this.searchType === SearchType.DATE;
   }
 
   /**
@@ -43,15 +68,21 @@ export class EventsSearchComponent implements OnInit {
   searchDate(): void {
     if (this.dateForm.valid) {
       const date = new Date(this.dateForm.get('startDate').value);
-
       const search = SearchEvent.fromDate(date);
-      this.eventService.searchEvents(search).subscribe((events: Event[]) => {
-        this.searchedEvents = events;
-        this.searched = true;
-      },
-        (e: HttpErrorResponse) => {
-          console.log(e);
-        });
+      this.doSearch(search);
+    }
+  }
+
+  /**
+   * Perform search request
+   * @param query object with query parameters
+   */
+  private doSearch(query: SearchEvent): void {
+    this.eventService.searchEvents(query).subscribe((events: Event[]) => {
+      this.searchedEvents = events;
+      this.searched = true;
+    }), (e: HttpErrorResponse) => {
+      console.log(e);
     }
   }
 }
