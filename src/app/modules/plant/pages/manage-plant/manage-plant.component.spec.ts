56a58bdf1f70337be9dacf3cb7c3c42a706c6b06
Tilkeii <florian@gouarin.info>
2020-06-23 20:00:14 +0200

    test(plant): fix test

--- src/app/modules/plant/pages/manage-plant/manage-plant.component.spec.ts
+++ src/app/modules/plant/pages/manage-plant/manage-plant.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ManagePlantComponent } from './manage-plant.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { PlantService } from 'src/app/core/services/plant/plant.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ManagePlantComponent', () => {
   let component: ManagePlantComponent;
@@ -8,7 +14,16 @@ describe('ManagePlantComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ManagePlantComponent ]
+      imports: [
+        RouterTestingModule,
+        NzGridModule
+      ],
+      declarations: [ ManagePlantComponent ],
+      providers: [
+        { provide: PlantService, useValue: {} },
+        { provide: ActivatedRoute, useValue: {queryParams: of({ page: 1 })} }
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
     .compileComponents();
   }));
--- src/app/modules/plant/pages/my-garden/my-garden.component.spec.ts
+++ src/app/modules/plant/pages/my-garden/my-garden.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyGardenComponent } from './my-garden.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { PlantService } from 'src/app/core/services/plant/plant.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('MyGardenComponent', () => {
   let component: MyGardenComponent;
@@ -8,7 +14,16 @@ describe('MyGardenComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyGardenComponent ]
+      imports: [
+        RouterTestingModule,
+        NzGridModule
+      ],
+      declarations: [ MyGardenComponent ],
+      providers: [
+        { provide: PlantService, useValue: {} },
+        { provide: ActivatedRoute, useValue: {queryParams: of({ page: 1 })} }
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
     .compileComponents();
   }));
