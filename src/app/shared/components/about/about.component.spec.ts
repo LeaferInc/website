a2a43901316ee4f8a11400c04d5a22584f981e6f
ddaninthe <d.daninthelopez@gmail.com>
2020-07-11 22:01:08 +0200

    Added Logo brand on navbar

--- src/app/shared/components/about/about.component.spec.ts
+++ src/app/shared/components/about/about.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -8,7 +10,11 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [ AboutComponent ],
+      imports: [
+        MarkdownModule.forRoot(),
+        NzGridModule,
+      ],
     })
     .compileComponents();
   }));
